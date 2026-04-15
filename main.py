242780117902e7349b4a92ae7ef964938e331681
Sevinch <ysevinchoy2197@gmail.com>
2026-04-15 17:29:21 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+#2-misol
+class telefon:
+    brand = "Samsung"
+
+s1 = telefon()
+print(f"s1 brand: {s1.brand}")
+
+s2 = telefon()
+s2.narx = "1000"
+print(f"s1 narx: {s2.narx}")
+
+s3 = telefon()
+s3.xotira = "120"
+print(f"s3 xotira: {s3.xotira}")
+
+#3-misol
+class kitob:
+    sahifa = 200
+
+s1 = kitob()
+print(f"s1 sahifa: {s1.sahifa}")
+
+s2 = kitob()
+s2.nomi = "Dunyoning ishlari"
+print(f"s2 nomi: {s2.nomi}")
+
+s3 = kitob()
+s3.muallif = "O'tkir Xoshimov"
+print(f"s3 muallif: {s3.muallif}")
+
+#4-misol
+class talaba:
+    kurs = 1
+
+s1 = talaba()
+print(f"s1 kurs: {s1.kurs}")
+
+s2 = talaba()
+s2.ism = "Sevinch"
+print(f"s2 ism: {s2.ism}")
+
+s3 = talaba()
+s3.yosh = 15
+print(f"s3 yosh: {s3.yosh}")
+
+#5-misol
+class kompyuter:
+    ram = 8
+s1 = kompyuter()
+print(f"s1 ram: {s1.ram}")
+
+s2 = kompyuter()
+s2.protsessor = "Python"
+print(f"s2 protsessor: {s2.protsessor}")
+
+s3 = kompyuter()
+s3.ssd = 15
+print(f"s3 ssd: {s3.ssd}")
